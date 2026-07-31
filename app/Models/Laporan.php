46953aa28da0d41a2ff6e496124ab3941be1be55
Ahnaf Muafa <ahnaf.src@gmail.com>
2026-07-31 20:26:23 +0700

    feat: add Sanggahan model and establish relationships in Laporan model

--- app/Models/Laporan.php
+++ app/Models/Laporan.php
@@ -8,4 +8,18 @@ class Laporan extends Model
 {
     //
     protected $table = "laporan";
+
+    protected $fillable = ['ruangan_id', 'kelas_terduga_id', 'nama_pelapor', 'kelas_pelapor', 'foto', 'waktu_lapor', 'status', 'catatan_koreksi'];
+
+    public function Ruangan() {
+        return $this->belongsTo(Ruangan::class, 'ruangan_id');
+    }
+
+    public function KelasTerduga(){
+        return $this->belongsTo(Kelas::class, 'kelas_terduga_id');
+    }
+
+    public function Sanggahan(){
+        return $this->hasOne(Sanggahan::class, 'laporan_id');
+    }
 }
---
+++ app/Models/Sanggahan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sanggahan extends Model
+{
+    //
+
+    protected $table = 'sanggahan';
+}
